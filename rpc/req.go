f4d7201f4e3dc147f34565cf5e344ddaade7ceae
pengjiangyu <wuzhengsheng@dartou.com>
2022-09-02 14:37:27 +0800

    feat(rpc):request 添加发送字节方法

--- rpc/req.go
+++ rpc/req.go
@@ -13,20 +13,24 @@ var (
 	requestByCallID sync.Map
 )
 
-type request struct {
+type Request struct {
 	id     int64
 	onRecv func(interface{})
 }
 
 var ErrTimeout = errors.New("RPC time out")
 
-func (self *request) RecvFeedback(msg interface{}) {
+func (self *Request) RecvFeedback(msg interface{}) {
 
 	// 异步和同步执行复杂，队列处理在具体的逻辑中手动处理
 	self.onRecv(msg)
 }
 
-func (self *request) Send(ses cellnet.Session, msg interface{}) {
+func (self *Request) GetId() int64 {
+	return self.id
+}
+
+func (self *Request) Send(ses cellnet.Session, msg interface{}) {
 
 	//ctx, _ := ses.(cellnet.ContextSet)
 
@@ -46,9 +50,21 @@ func (self *request) Send(ses cellnet.Session, msg interface{}) {
 	//codec.FreeCodecResource(meta.Codec, data, ctx)
 }
 
-func CreateRequest(onRecv func(interface{})) *request {
+func (self *Request) SendByte(ses cellnet.Session, msg []byte) {
 
-	self := &request{
+	//ctx, _ := ses.(cellnet.ContextSet)
+
+	ses.Send(&RemoteCallREQ{
+		Data:   msg,
+		CallID: self.id,
+	})
+
+	//codec.FreeCodecResource(meta.Codec, data, ctx)
+}
+
+func CreateRequest(onRecv func(interface{})) *Request {
+
+	self := &Request{
 		onRecv: onRecv,
 	}
 
@@ -59,12 +75,12 @@ func CreateRequest(onRecv func(interface{})) *request {
 	return self
 }
 
-func GetRequest(callid int64) *request {
+func GetRequest(callid int64) *Request {
 
 	if v, ok := requestByCallID.Load(callid); ok {
 
 		requestByCallID.Delete(callid)
-		return v.(*request)
+		return v.(*Request)
 	}
 
 	return nil
